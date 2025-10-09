bdf8f596feb857b4ca8a3639882f61d1e0050feb
Dian Ramdhani Lubis <dian.ramdhani@tritronik.com>
2025-10-09 11:41:29 +0700

    feat(plugins): add Indonesian tutor commands
    - add TutorID1, TutorID2, TutorID3, TutorID4 commands to open Indonesian tutor files
    - add TutorList command to display available Indonesian tutorials
    - create indonesia-01-dasar.txt: Basic Lesson
    - create indonesia-02-editing.txt: Basic Text Editing
    - create indonesia-03-gerakan.txt: Advanced Movement
    - create indonesia-04-visual.txt: Visual Mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,27 @@
+vim.api.nvim_create_user_command("TutorID1", function()
+  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/lua/plugins/tutor/id/indonesia-01-dasar.txt")
+end, { desc = "Buka Tutorial Dasar Bahasa Indonesia" })
+
+vim.api.nvim_create_user_command("TutorID2", function()
+  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/lua/plugins/tutor/id/indonesia-02-editing.txt")
+end, { desc = "Buka Tutorial Editing Bahasa Indonesia" })
+
+vim.api.nvim_create_user_command("TutorID3", function()
+  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/lua/plugins/tutor/id/indonesia-03-gerakan.txt")
+end, { desc = "Buka Tutorial Gerakan Bahasa Indonesia" })
+
+vim.api.nvim_create_user_command("TutorID4", function()
+  vim.cmd("edit " .. vim.fn.stdpath("config") .. "/lua/plugins/tutor/id/indonesia-04-visual.txt")
+end, { desc = "Buka Tutorial Visual Bahasa Indonesia" })
+
+vim.api.nvim_create_user_command("TutorList", function()
+  print("📚 TUTORIAL BAHASA INDONESIA:")
+  print(":TutorID1 - Tutorial Dasar Vim")
+  print(":TutorID2 - Tutorial Editing Teks")
+  print(":TutorID3 - Tutorial Gerakan Lanjutan") 
+  print(":TutorID4 - Tutorial Mode Visual")
+end, { desc = "Lihat daftar tutorial Indonesia" })
+
 return {
   {
     "stevearc/conform.nvim",
